609092faa919acd63f698bab5a0decfb5a4e66a8
gomez-code <gomez.bdlm@gmail.com>
2023-07-10 17:02:58 -0300

    menu arreglado

--- javascript.js
+++ javascript.js
@@ -1,26 +1,10 @@
-// Obtener el elemento nav y su elemento padre
-const nav = document.querySelector('nav');
-const parentElement = nav.parentElement;
-
-// Observar cambios en el color de fondo del elemento padre
-const observer = new MutationObserver(() => {
-  const backgroundColor = getComputedStyle(parentElement).backgroundColor;
-  const isLightBackground = isLightColor(backgroundColor);
-
-  // Ajustar el color del texto según el fondo
-  if (isLightBackground) {
-    nav.classList.add('dark-text');
-  } else {
-    nav.classList.remove('dark-text');
-  }
+document.querySelector('#nav-bootstrap .navbar-toggler').addEventListener('click', function() {
+  document.querySelector('#nav-bootstrap').classList.toggle('menu-opened');
 });
 
-// Configurar y comenzar la observación del color de fondo
-observer.observe(parentElement, { attributes: true });
-
-// Función para verificar si un color es claro u oscuro
-function isLightColor(color) {
-  const rgb = color.match(/\d+/g).map(Number);
-  const brightness = (rgb[0] * 299 + rgb[1] * 587 + rgb[2] * 114) / 1000;
-  return brightness > 125;
-}
+document.querySelectorAll('#nav-bootstrap .offcanvas-header .btn-close').forEach(function(button) {
+  button.addEventListener('click', function() {
+    document.querySelector('#nav-bootstrap').classList.remove('menu-opened');
+    window.scrollTo(0, 0);
+  });
+});
